871f5c4562592b4031114f01ed30900d041127bb
Jean Claude <jeanyao@ymail.com>
2024-02-13 10:06:07 +0500

    Migration and Schema

---
+++ src/Component/Database/Migration/Contract/MigrationInterface.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Laventure\Component\Database\Migration\Contract;
+
+
+use Laventure\Component\Database\Schema\Contract\SchemaInterface;
+
+/**
+ * MigrationInterface
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  Laventure\Component\Database\Migration\Contract
+*/
+interface MigrationInterface
+{
+
+    /**
+     * Create or modify schema table
+     *
+     * @param SchemaInterface $schema
+     *
+     * @return void
+    */
+    public function up(SchemaInterface $schema): void;
+
+
+
+
+
+
+    /**
+     * Drop schema table or others modification
+     *
+     * @param SchemaInterface $schema
+     * @return void
+    */
+    public function down(SchemaInterface $schema): void;
+}
---
+++ src/Component/Database/Schema/Contract/SchemaInterface.php
@@ -0,0 +1,175 @@
+<?php
+declare(strict_types=1);
+
+namespace Laventure\Component\Database\Schema\Contract;
+
+
+/**
+ * SchemaInterface
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  Laventure\Component\Database\Schema\Contract
+ */
+interface SchemaInterface
+{
+    /**
+     * Create schema
+     *
+     * @param string $table
+     *
+     * @param Closure $closure
+     *
+     * @return mixed
+     */
+    public function create(string $table, Closure $closure): mixed;
+
+
+
+
+
+
+    /**
+     * Update schema
+     *
+     * @param string $table
+     *
+     * @param Closure $closure
+     *
+     * @return mixed
+     */
+    public function update(string $table, Closure $closure): mixed;
+
+
+
+
+
+
+    /**
+     * Drop schema
+     *
+     * @param string $table
+     *
+     * @return mixed
+     */
+    public function drop(string $table): mixed;
+
+
+
+
+
+
+    /**
+     * Drop schema if exists
+     *
+     * @param string $table
+     *
+     * @return mixed
+     */
+    public function dropIfExists(string $table): mixed;
+
+
+
+
+
+    /**
+     * Truncate table
+     *
+     * @param string $table
+     *
+     * @return mixed
+     */
+    public function truncate(string $table): mixed;
+
+
+
+
+
+
+    /**
+     * Truncate table cascade
+     *
+     * @param string $table
+     *
+     * @return mixed
+    */
+    public function truncateCascade(string $table): mixed;
+
+
+
+
+
+
+
+    /**
+     * Returns table columns
+     *
+     * @param string $table
+     *
+     * @return array
+    */
+    public function getColumns(string $table): array;
+
+
+
+
+
+
+
+    /**
+     * Determine if schema exists
+     *
+     * @param string $table
+     *
+     * @return bool
+    */
+    public function exists(string $table): bool;
+
+
+
+
+
+
+
+
+    /**
+     * Determine if columns exists in given table
+     *
+     * @param string $table
+     *
+     * @param string $column
+     *
+     * @return bool
+    */
+    public function hasColumn(string $table, string $column): bool;
+
+
+
+
+
+
+    /**
+     * @param string $sql
+     *
+     * @return mixed
+    */
+    public function exec(string $sql): mixed;
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Return database tables
+     *
+     * @return array
+    */
+    public function getTables(): array;
+}
