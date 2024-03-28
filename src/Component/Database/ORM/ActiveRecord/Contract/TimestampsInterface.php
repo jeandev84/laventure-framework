4d0a38705ca916dbf4d504eebb0a714cbbe50171
Jean Claude <jeanyao@ymail.com>
2024-03-28 12:10:57 +0500

    Timestamps

---
+++ src/Component/Database/ORM/ActiveRecord/Contract/TimestampsInterface.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Laventure\Component\Database\ORM\ActiveRecord\Contract;
+
+
+/**
+ * TimestampsInterface
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  Laventure\Component\Database\ORM\ActiveRecord\Contract
+*/
+interface TimestampsInterface
+{
+    /**
+     * Returns timestamp like created_at, updated_at
+     *
+     * @return string[]
+    */
+    public function getTimestamps(): array;
+}
